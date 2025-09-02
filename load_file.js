3219ccb80d2a6f11a3d40ba7d5cd66a3b78fb883
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-09-02 18:24:08 +0800

    improved file structuring
    broken project section, needs fixing.

---
+++ load_file.js
@@ -0,0 +1,13 @@
+async function loadHTML(getId, getFile) {
+  const response = await fetch(getFile)
+  const text = await response.text()
+  document.getElementById(getId).innerHTML = text
+}
+
+loadHTML("nav", String.raw`template\nav.html`)
+loadHTML("profile", String.raw`template\profile.html`)
+loadHTML("about", String.raw`template\about.html`)
+loadHTML("experience", String.raw`template\experience.html`)
+loadHTML("project", String.raw`template\project.html`)
+loadHTML("contact", String.raw`template\contact.html`)
+loadHTML("footer", String.raw`template\footer.html`)
